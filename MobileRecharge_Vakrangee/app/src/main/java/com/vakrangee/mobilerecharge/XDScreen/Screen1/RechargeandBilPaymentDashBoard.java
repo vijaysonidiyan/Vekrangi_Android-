a4be04aa8888f2b53a7e3696598d3c6247583a3e
Harshil Thakkar <harshilthakkar@diyantech.in>
2022-07-27 17:13:07 +0530

    Package setup

---
+++ MobileRecharge_Vakrangee/app/src/main/java/com/vakrangee/mobilerecharge/XDScreen/Screen1/RechargeandBilPaymentDashBoard.java
@@ -0,0 +1,16 @@
+package com.vakrangee.mobilerecharge.XDScreen.Screen1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vakrangee.mobilerecharge.R;
+
+public class RechargeandBilPaymentDashBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rechargeand_bil_payment_dash_board);
+    }
+}
