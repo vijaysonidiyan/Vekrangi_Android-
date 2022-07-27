e7de5cb2ece42b36c7e7aed4c207426c2e7973c4
Anisha <101786317+anishaa123@users.noreply.github.com>
2022-07-27 19:05:39 +0530

    Mobile Recharge UI
    - create UI for 51, 52 page.

---
+++ MobileRecharge_Vakrangee/app/src/main/java/com/vakrangee/mobilerecharge/electricityBill/activities/BaseActivity.java
@@ -0,0 +1,23 @@
+package com.vakrangee.mobilerecharge.electricityBill.activities;
+
+
+//public abstract class BaseActivity<BINDING extends ViewBinding, VM extends BaseViewModel> extends AppCompatActivity {
+//
+//    protected VM viewModel;
+//    protected BINDING binding;
+//
+//    @NonNull
+//    protected abstract VM createViewModel();
+//
+//    @NonNull
+//    protected abstract BINDING createViewBinding(LayoutInflater layoutInflater);
+//
+//    @Override
+//    protected void onCreate(final Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        binding = createViewBinding(LayoutInflater.from(this));
+//        setContentView(binding.getRoot());
+//        viewModel = createViewModel();
+//    }
+//
+//}
---
+++ MobileRecharge_Vakrangee/app/src/main/java/com/vakrangee/mobilerecharge/electricityBill/models/BaseViewModel.java
@@ -0,0 +1,6 @@
+package com.vakrangee.mobilerecharge.electricityBill.models;
+
+//import android.arch.lifecycle.ViewModel;
+
+//public abstract class BaseViewModel extends ViewModel {
+//}
