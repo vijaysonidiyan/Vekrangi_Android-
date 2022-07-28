b2378493468beb78825f6b69beee84d13fdb02a6
Harshil Thakkar <harshilthakkar@diyantech.in>
2022-07-28 11:58:46 +0530

    screen0.2

---
+++ MobileRecharge_Vakrangee/app/src/main/java/com/vakrangee/mobilerecharge/MobileRecharge/ContactActivity.java
@@ -0,0 +1,15 @@
+package com.vakrangee.mobilerecharge.MobileRecharge;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vakrangee.mobilerecharge.R;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
