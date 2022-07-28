f82c4e3c46b352ca67e5ed061166a71cfef3d6b5
Harshil Thakkar <harshilthakkar@diyantech.in>
2022-07-28 18:52:32 +0530

    screen10

--- MobileRecharge_Vakrangee/app/src/main/java/com/vakrangee/mobilerecharge/MobileRecharge/Confirm_OperatorActivity.java
+++ MobileRecharge_Vakrangee/app/src/main/java/com/vakrangee/mobilerecharge/MobileRecharge/Confirm_OperatorActivity.java
@@ -3,14 +3,54 @@ package com.vakrangee.mobilerecharge.MobileRecharge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.vakrangee.mobilerecharge.R;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Confirm_OperatorActivity extends AppCompatActivity {
+    private EditText EditNumber;
+    private Spinner SpinnerJio;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_operator);
+        initialization();
     }
+
+    private void initialization() {
+        EditNumber = findViewById(R.id.EditNumber);
+        SpinnerJio = findViewById(R.id.SpinnerJio);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.dropdwon_jio, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        SpinnerJio.setAdapter(adapter);
+
+//        if (!isValidPhone(EditNumber.getText().toString())) {
+//            EditNumber.setError("Enter valid email");
+//
+//        }
+    }
+
+
+//    private boolean isValidPhone(String phone) {
+//        Pattern pattern;
+//        Matcher matcher;
+//
+//        final String PHONE_PATTERN= "[0-9]{10}";
+//
+//        pattern = Pattern.compile(PHONE_PATTERN);
+//        matcher = pattern.matcher(phone);
+//        return matcher.matches();
+//
+//    }
 }
---
+++ MobileRecharge_Vakrangee/app/src/main/java/com/vakrangee/mobilerecharge/MobileRecharge/HelpActivity.java
@@ -0,0 +1,16 @@
+package com.vakrangee.mobilerecharge.MobileRecharge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vakrangee.mobilerecharge.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
