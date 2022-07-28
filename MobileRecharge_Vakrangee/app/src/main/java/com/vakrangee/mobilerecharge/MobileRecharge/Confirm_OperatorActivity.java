dc84b39b3afe45865a4049b0adda9d1f769c6bb4
Harshil Thakkar <harshilthakkar@diyantech.in>
2022-07-28 12:01:27 +0530

    Screen0.4

---
+++ MobileRecharge_Vakrangee/app/src/main/java/com/vakrangee/mobilerecharge/MobileRecharge/Confirm_OperatorActivity.java
@@ -0,0 +1,16 @@
+package com.vakrangee.mobilerecharge.MobileRecharge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vakrangee.mobilerecharge.R;
+
+public class Confirm_OperatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_operator);
+    }
+}
